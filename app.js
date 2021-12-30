f4c52e6946cc6c440af501b697ae86081c551353
Jannatul Naim <69507020+jannatul29@users.noreply.github.com>
2021-12-30 19:09:51 +0600

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const rateLimit = require("express-rate-limit");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+const limitReached = (req, res)=> {
+  //res.render("index", {title:"Forbidden 403"});
+  res.send('Too many request from this IP');
+  }
+
+const limiter = rateLimit({
+  windowMs: 10000,
+  max: 2,
+  message: "Too many request from this IP",
+  resetTime: 10000,
+  //handler: limitReached,
+  });
+
+app.use(limiter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +35,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/',limiter, indexRouter);
+//app.use('/',indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
